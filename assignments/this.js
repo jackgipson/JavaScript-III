9c96bc4523ed8a7d188084ab03ed6ee45f064820
jackgipson <jackjgipson@gmail.com>
2019-02-05 11:25:38 -0800

    finished the this file

--- assignments/this.js
+++ assignments/this.js
@@ -30,11 +30,45 @@ myFunction();
 
 // code example for Implicit Binding
 
+const polarBear = {
+    name: 'Chuck',
+    food: 'fish',
+    cook: function() {
+     console.log(`${this.name} loves to eat ${this.food}`) 
+    }
+  }
+  
+   polarBear.cook();  
+
 // Principle 3
 
 // code example for New Binding
+
+function GrizzlyBear(name, age) {
+  
+    this.name = name
+    this.age = age
+  }
+  
+  const friend = new GrizzlyBear('Bubba', 67);
+  console.log(friend);
 
 // Principle 4
 
 
 // code example for Explicit Binding
+
+const skills = ["igloo\'s'", "drink martini\'s'", "sleep"];
+
+function greet (skills1, skills2, skills3) {
+     console.log(`Hello Grizzly Bear my name is ${this.name} and I love to eat ${this.food} and make ${skills1}, ${skills2} and ${skills3}.`) 
+    };
+
+const polarBear = {
+    name: 'Chuck',
+    food: 'fish' 
+};
+  
+  
+  greet.call(polarBear, skills); // .call does not separate the commas either
+  greet.apply(polarBear, skills);
